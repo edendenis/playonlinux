801b60b7118647c2ee5ea55fa722102effd5ebec
Eden Denis <edendenis@github.com>
2026-02-27 20:41:12 -0300

    updating README

--- README.py
+++ README.py
@@ -16,36 +16,67 @@
 # 
 # ### `PlayOnLinux (POL)`
 # 
-# O `PlayOnLinux (POL)` é uma aplicação de código aberto projetada para simplificar a instalação e execução de jogos e aplicativos do `Windows` em sistemas Linux. Ele utiliza o Wine, uma camada de compatibilidade que permite a execução de programas `Windows` em ambientes Linux, e oferece uma interface gráfica amigável que facilita o processo de configuração e instalação. O PlayOnLinux possui um vasto repositório de scripts e configurações pré-definidas para uma ampla variedade de jogos e programas, tornando mais fácil para os usuários encontrar e instalar suas aplicações `Windows` favoritas. Isso o torna uma escolha popular para jogadores e usuários que precisam de aplicativos específicos do `Windows` em seus sistemas `Linux`, proporcionando uma experiência mais acessível e otimizada.
+# O `PlayOnLinux (POL)` é uma aplicação de código aberto projetada para simplificar a instalação e execução de jogos e aplicativos do `Windows` em sistemas `Linux`. Ele utiliza o Wine, uma camada de compatibilidade que permite a execução de programas `Windows` em ambientes `Linux`, e oferece uma interface gráfica amigável que facilita o processo de configuração e instalação. O PlayOnLinux possui um vasto repositório de _scripts_ e configurações pré-definidas para uma ampla variedade de jogos e programas, tornando mais fácil para os usuários encontrar e instalar suas aplicações `Windows` favoritas. Isso o torna uma escolha popular para jogadores e usuários que precisam de aplicativos específicos do `Windows` em seus sistemas `Linux`, proporcionando uma experiência mais acessível e otimizada.
 # 
 
 # ## 1. Configurar/Instalar/Usar o `PlayOnLinux (POL)` no `Linux Ubuntu` [1]
 # 
 # Para configurar/instalar/usar o `PlayOnLinux (POL)` no `Linux Ubuntu`, você pode seguir estas etapas:
 # 
-# 1. Abra o `Terminal Emulator`. Você pode fazer isso pressionando: `Ctrl + Alt + T`
+# 1. Abrir o `Terminal Emulator`. Você pode fazer isso pressionando:
+# 
+#     ```bash
+#     Ctrl + Alt + T
+#     ```
 # 
 
 # 2. Certifique-se de que seu sistema esteja limpo e atualizado.
 # 
-#     2.1 Limpar o `cache` do gerenciador de pacotes `apt`. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
-#     
-#     2.2 Remover pacotes `.deb` antigos ou duplicados do cache local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
+#     2.1 Limpar o `cache` do gerenciador de pacotes `apt`. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando:
+#     ```bash
+#     sudo apt clean
+#     ```
 # 
-#     2.3 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando: `sudo apt autoremove -y`
+#     2.2 Remover pacotes `.deb` antigos ou duplicados do `cache` local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando:
+#     ```bash
+#     sudo apt autoclean
+#     ```
 # 
-#     2.4 Buscar as atualizações disponíveis para os pacotes que estão instalados em seu sistema. Digite o seguinte comando e pressione `Enter`: `sudo apt update`
+#     2.3 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando:
+#     ```bash
+#     sudo apt autoremove -y
+#     ```
 # 
-#     2.5 **Corrigir pacotes quebrados**: Isso atualizará a lista de pacotes disponíveis e tentará corrigir pacotes quebrados ou com dependências ausentes: `sudo apt --fix-broken install`
+#     2.4 Buscar as atualizações disponíveis para os pacotes que estão instalados em seu sistema. Digite o seguinte comando e pressione `Enter`:
+#     ```bash
+#     sudo apt update
+#     ```
 # 
-#     2.6 Limpar o `cache` do gerenciador de pacotes `apt`. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
-#     
-#     2.7 Para ver a lista de pacotes a serem atualizados, digite o seguinte comando e pressione `Enter`:  `sudo apt list --upgradable`
+#     2.5 **Corrigir pacotes quebrados**: Isso atualizará a lista de pacotes disponíveis e tentará corrigir pacotes quebrados ou com dependências ausentes:
+#     ```bash
+#     sudo apt --fix-broken install
+#     ```
 # 
-#     2.8 Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update`. Digite o seguinte comando e pressione `Enter`: `sudo apt full-upgrade -y`
-#     
+#     2.6 Limpar o `cache` do gerenciador de pacotes `apt` novamente:
+#     ```bash
+#     sudo apt clean
+#     ```
+# 
+#     2.7 Para ver a lista de pacotes a serem atualizados, digite o seguinte comando e pressione `Enter`:
+#     ```bash
+#     sudo apt list --upgradable
+#     ```
+# 
+#     2.8 Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update`. Digite o seguinte comando e pressione `Enter`:
+#     ```bash
+#     sudo apt full-upgrade -y
+#     ```
 
-# 3. Para configurar/instalar/usar o `PlayOnLinux (POL)` execute o comando: `sudo apt install playonlinux -y`
+# 3. Para configurar/instalar/usar o `PlayOnLinux (POL)` execute o comando:
+# 
+#     ```bash
+#     sudo apt install playonlinux -y
+#     ```
 # 
 #     **OBSERVAÇÂO(ÕES)**: O diretório para a(s) pasta(s) ou arquivo(s) é indicado a seguir, conferir se no diretório se a(s) pasta(s) e/ou o(s) arquivo(s) existe(m), se não, copiar da pasta `docs` para o diretório:
 #     
@@ -67,11 +98,15 @@
 # 
 # Para configurar/instalar/usar o `PlayOnLinux (POL)` no `Linux Ubuntu` sem precisar digitar linha por linha, você pode seguir estas etapas:
 # 
-# 1. Abra o `Terminal Emulator`. Você pode fazer isso pressionando: `Ctrl + Alt + T`
+# 1. Abrir o `Terminal Emulator`. Você pode fazer isso pressionando:
+# 
+#     ```bash
+#     Ctrl + Alt + T
+#     ```
 # 
 # 2. Digite o seguinte comando e pressione `Enter`:
 # 
-#     ```
+#     ```bash
 #     sudo apt autoclean
 #     sudo apt autoremove -y
 #     sudo apt update
@@ -85,7 +120,7 @@
 
 # ## Referências
 # 
-# [1] OPENAI. ***Instalação do playonlinux no xubuntu.*** Disponível em: <https://chat.openai.com/c/74f27d86-e06e-4724-8c5a-e8fe2aed3754> (texto adaptado). Acessado em: 20/10/2023 22:16.
+# [1] OPENAI. ***Instalar o `playonlinux` no `linux ubuntu` pelo `terminal emulator`.*** Disponível em: <https://chat.openai.com/c/74f27d86-e06e-4724-8c5a-e8fe2aed3754> (texto adaptado). Acessado em: 20/10/2023 22:16.
 # 
 # [2] OPENAI. ***Vs code: editor popular.*** Disponível em: <https://chat.openai.com/c/b640a25d-f8e3-4922-8a3b-ed74a2657e42> (texto adaptado). Acessado em: 14/11/2023 09:33.
 # 
